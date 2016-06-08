0a0d96dcc0892536eafa1790e4c51ffbfb528091
Felix Gabel <felix.gabel@me.com>
2016-06-08 23:42:04 +0200

    Add Blending

---
+++ Sources/Blending.swift
@@ -0,0 +1,31 @@
+
+public enum Blending {
+    case Normal
+    case Multiply
+    case Screen
+    case Overlay
+    case Darken
+    case Lighten
+    case ColorDodge
+    case ColorBurn
+    case SoftLight
+    case HardLight
+    case Difference
+    case Exclusion
+    case Hue
+    case Saturation
+    case Color
+    case Luminosity
+    case Clear
+    case Copy
+    case SourceIn
+    case SourceOut
+    case SourceAtop
+    case DestinationOver
+    case DestinationIn
+    case DestinationOut
+    case DestinationAtop
+    case XOR
+    case PlusDarker
+    case PlusLighter
+}
